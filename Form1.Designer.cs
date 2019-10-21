b4484693cf28df44dccb79e50341c19bdce015f9
qwastoch <tochenov235@ya.ru>
2019-10-21 11:21:37 +0300

    no timer and no pause

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,22 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // button1
@@ -58,7 +54,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(218, 176);
+            this.button2.Location = new System.Drawing.Point(305, 186);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -66,21 +62,10 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
-            // button3
-            // 
-            this.button3.Enabled = false;
-            this.button3.Location = new System.Drawing.Point(335, 176);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Пауза";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.Button3_Click);
-            // 
             // button4
             // 
             this.button4.Enabled = false;
-            this.button4.Location = new System.Drawing.Point(439, 176);
+            this.button4.Location = new System.Drawing.Point(432, 186);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(97, 23);
             this.button4.TabIndex = 3;
@@ -120,20 +105,10 @@
             this.textBox3.TabIndex = 9;
             this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(583, 181);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 13);
-            this.label1.TabIndex = 10;
-            this.label1.Text = "Время: 00:00:00:00";
-            this.label1.Click += new System.EventHandler(this.Label1_Click);
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(204, 207);
+            this.label2.Location = new System.Drawing.Point(204, 219);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 11;
@@ -171,10 +146,6 @@
             // 
             this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.FolderBrowserDialog1_HelpRequest);
             // 
-            // timer1
-            // 
-            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -184,13 +155,11 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.treeView1);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -205,19 +174,16 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
-        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,13 +13,9 @@ namespace MySearchEngine
 {
     public partial class Form1 : Form
     {
-        DateTime date = DateTime.Now;
-        Timer timer = new Timer();
         public Form1()
         {
             InitializeComponent();
-            timer1.Interval = 10;
-            timer1.Tick += new EventHandler(tickTimer);
             textBox1.Text = "";
             textBox2.Text = "*.txt";
             textBox3.Text = "";
@@ -57,15 +53,13 @@ namespace MySearchEngine
             draft.WriteLine(textBox2.Text);
             draft.WriteLine(textBox3.Text);
             draft.Close();
-            timer1.Enabled = true;
             textBox1.ReadOnly = true;
             textBox2.ReadOnly = true;
             textBox3.ReadOnly = true;
-            button3.Enabled = true;
             button1.Enabled = false;
             button2.Enabled = false;
-            button2.Text = "Продолжить";
-            EnablePause = false;
+            
+            
             try
             {
                 DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
@@ -74,47 +68,30 @@ namespace MySearchEngine
             }
             catch (UnauthorizedAccessException ex)
             {
-                timer1.Enabled = false;
                 button4.Enabled = true;
                 button2.Enabled = false;
-                button3.Enabled = false;
                 MessageBox.Show(ex.Message);
             }
             catch (OutOfMemoryException ex)
             {
-                timer1.Enabled = false;
                 button4.Enabled = true;
                 button2.Enabled = false;
-                button3.Enabled = false;
                 MessageBox.Show(ex.Message);
             }
             
         }
 
-        private void Button3_Click(object sender, EventArgs e)
-        {
-            timer1.Enabled = false;
-            button4.Enabled = true;
-            button2.Enabled = true;
-            button3.Enabled = false;
-            EnablePause = true;
-           
-        }
+       
 
         private void Button4_Click(object sender, EventArgs e)
         {
-            search_time = new DateTime(0, 0);
-            label1.Text = "Время: " + "00:00:00:00";
-            timer1.Enabled = false;
+            
             textBox1.ReadOnly = false;
             textBox2.ReadOnly = false;
             textBox3.ReadOnly = false;
             button1.Enabled = true;
-            button2.Text = "Старт";
             button2.Enabled = true;
-            button3.Enabled = false;
             button4.Enabled = false;
-            EnablePause = false;
             label2.Text = "";
             treeView1.Nodes.Clear();
         }
@@ -139,11 +116,6 @@ namespace MySearchEngine
 
         }
 
-        private void Label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void Label2_Click(object sender, EventArgs e)
         {
 
@@ -152,18 +124,6 @@ namespace MySearchEngine
         private void FolderBrowserDialog1_HelpRequest(object sender, EventArgs e)
         {
 
-        }
-        DateTime search_time = new DateTime(0, 0);
-        private void Timer1_Tick(object sender, EventArgs e)
-        {
-            
-        }
-        private void tickTimer(object sender, EventArgs e)
-        {
-            long tick = DateTime.Now.Ticks - date.Ticks;
-            DateTime stopWatch = new DateTime();
-            stopWatch = stopWatch.AddTicks(tick);
-            label1.Text = "Время: " + String.Format("{0:HH:mm:ss:ff}", stopWatch);
         }
 
         private void Label4_Click(object sender, EventArgs e)
@@ -181,41 +141,25 @@ namespace MySearchEngine
             {
 
                 Tree(subdir, curNode.Nodes);
-                MyPause();
+                
             }
 
             foreach (FileInfo file in directoryinfo.GetFiles(textBox2.Text))
             {
                 label2.Text = file.FullName;
-                MyPause();
+                
 
                 if (File.ReadAllText(file.FullName).Contains(textBox3.Text))
                 {
                     curNode.Nodes.Add(file.Name);
-                    MyPause();
+                    
                 }
 
             }
-            timer1.Enabled = false;
             button2.Enabled = false;
-            button3.Enabled = false;
             button4.Enabled = true;
         }
-        bool EnablePause = false;
-        private void MyPause()
-        {
-            if (EnablePause == true)
-            {
-                for (int i = 0; i>0; i++ )
-                { int pause = 0;
-                    pause++;
-                    if (EnablePause == false)
-                    {
-                    break;
-                    }
-                }
-            } 
-
-        }
+        
+        
     }
 }
